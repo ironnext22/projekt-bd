194e1aae06f7646ab397f03a3bc802443ede0ae6
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-05-22 11:11:18 +0200

    Add files via upload

--- bazy projekt.sql
+++ bazy projekt.sql
@@ -73,9 +73,27 @@ CREATE TABLE `filmy` (
 
 LOCK TABLES `filmy` WRITE;
 /*!40000 ALTER TABLE `filmy` DISABLE KEYS */;
-INSERT INTO `filmy` VALUES (1,'Iron Man',1,1,1,1),(4,'aaa',1,1,NULL,NULL),(5,'Batman: Początek',2,1,2,2),(6,'Iron Man 2',1,1,1,1),(7,'Iron Man 3',1,1,1,1),(8,'Hobbit: Niezwykła Podróż',6,2,2,10),(9,'Harry Potter i Kamień Filozoficzny',8,2,2,11),(10,'Harry Potter i Komnata Tajemnic',8,2,2,11),(11,'Harry Potter i Więzień Azkabanu',8,2,2,11);
+INSERT INTO `filmy` VALUES (1,'Iron Man',1,1,1,1),(5,'Batman: Początek',2,1,2,2),(6,'Iron Man 2',1,1,1,1),(7,'Iron Man 3',1,1,1,1),(8,'Hobbit: Niezwykła Podróż',6,2,2,10),(9,'Harry Potter i Kamień Filozoficzny',8,2,2,11),(10,'Harry Potter i Komnata Tajemnic',8,2,2,11),(11,'Harry Potter i Więzień Azkabanu',8,2,2,11);
 /*!40000 ALTER TABLE `filmy` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `filmy2`
+--
+
+DROP TABLE IF EXISTS `filmy2`;
+/*!50001 DROP VIEW IF EXISTS `filmy2`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `filmy2` (
+  `id` tinyint NOT NULL,
+  `tytul` tinyint NOT NULL,
+  `gatunek` tinyint NOT NULL,
+  `reżyser` tinyint NOT NULL,
+  `wytwórnia` tinyint NOT NULL,
+  `rola_główna` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Temporary table structure for view `filmyl`
@@ -106,7 +124,7 @@ CREATE TABLE `gatunek` (
   `nazwa` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `gatunek_pk` (`nazwa`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,7 +133,7 @@ CREATE TABLE `gatunek` (
 
 LOCK TABLES `gatunek` WRITE;
 /*!40000 ALTER TABLE `gatunek` DISABLE KEYS */;
-INSERT INTO `gatunek` VALUES (1,'Akcji'),(7,'Dramat'),(2,'Fantasy'),(8,'Horror'),(11,'Kryminał'),(10,'Romans'),(6,'Sci-fi'),(9,'Thriller');
+INSERT INTO `gatunek` VALUES (1,'Akcji'),(12,'Anime'),(7,'Dramat'),(2,'Fantasy'),(8,'Horror'),(11,'Kryminał'),(10,'Romans'),(6,'Sci-fi'),(9,'Thriller');
 /*!40000 ALTER TABLE `gatunek` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,7 +168,7 @@ CREATE TABLE `klienci` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `imie` varchar(100) DEFAULT NULL,
   `nazwisko` varchar(100) DEFAULT NULL,
-  `pesel` int(11) DEFAULT NULL,
+  `pesel` mediumtext NOT NULL,
   `adres` varchar(100) DEFAULT NULL,
   `kod_pocztowy` varchar(6) DEFAULT NULL,
   `miejscowość` varchar(100) DEFAULT NULL,
@@ -166,7 +184,7 @@ CREATE TABLE `klienci` (
 
 LOCK TABLES `klienci` WRITE;
 /*!40000 ALTER TABLE `klienci` DISABLE KEYS */;
-INSERT INTO `klienci` VALUES (1,'Wiktor','Kowalski',2147483647,'ul. Warszawska','30-000','Kraków',300300300,'aaa@gmail.com'),(2,'Mateusz','Maź',2147483647,'ul. Poznańska 60','30-300','Kraków',666666666,'bbb@gmail.com'),(3,'Kacper','Filipek',111111111,'ul. Poznańska 20','30-300','Kraków',222222222,'ccc@gmail.com');
+INSERT INTO `klienci` VALUES (1,'Wiktor','Kowalski','33333333333','ul. Warszawska','30-000','Kraków',300300300,'aaa@gmail.com'),(2,'Mateusz','Maź','22222222222','ul. Poznańska 60','30-300','Kraków',666666666,'bbb@gmail.com'),(3,'Kacper','Filipek','11111111111','ul. Poznańska 20','30-300','Kraków',222222222,'ccc@gmail.com');
 /*!40000 ALTER TABLE `klienci` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -214,7 +232,7 @@ CREATE TABLE `wypożyczenia` (
   KEY `filmy` (`film`),
   CONSTRAINT `filmy` FOREIGN KEY (`film`) REFERENCES `filmy` (`id`),
   CONSTRAINT `klient` FOREIGN KEY (`klient`) REFERENCES `klienci` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,7 +241,7 @@ CREATE TABLE `wypożyczenia` (
 
 LOCK TABLES `wypożyczenia` WRITE;
 /*!40000 ALTER TABLE `wypożyczenia` DISABLE KEYS */;
-INSERT INTO `wypożyczenia` VALUES (1,1,1,'2023-05-20',7,50);
+INSERT INTO `wypożyczenia` VALUES (1,1,1,'2023-05-20',7,50),(2,5,2,'2023-05-21',7,25),(3,9,3,'2023-05-21',5,20),(4,8,1,'2023-05-21',4,16);
 /*!40000 ALTER TABLE `wypożyczenia` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -266,7 +284,7 @@ CREATE TABLE `zwroty` (
   PRIMARY KEY (`id`),
   KEY `wypożyczenie` (`wypożyczenie`),
   CONSTRAINT `wypożyczenie` FOREIGN KEY (`wypożyczenie`) REFERENCES `wypożyczenia` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -275,9 +293,28 @@ CREATE TABLE `zwroty` (
 
 LOCK TABLES `zwroty` WRITE;
 /*!40000 ALTER TABLE `zwroty` DISABLE KEYS */;
-INSERT INTO `zwroty` VALUES (1,1,'2023-05-28');
+INSERT INTO `zwroty` VALUES (1,1,'2023-05-28'),(2,3,'2023-05-21');
 /*!40000 ALTER TABLE `zwroty` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `filmy2`
+--
+
+/*!50001 DROP TABLE IF EXISTS `filmy2`*/;
+/*!50001 DROP VIEW IF EXISTS `filmy2`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `filmy2` AS select `filmy`.`id` AS `id`,`filmy`.`tytul` AS `tytul`,`gatunek`.`nazwa` AS `gatunek`,concat(`reżyser`.`Imie`,' ',`reżyser`.`nazwisko`) AS `reżyser`,`wytwórnia`.`nazwa` AS `wytwórnia`,concat(`aktor`.`imie`,' ',`aktor`.`nazwisko`) AS `rola_główna` from ((((`filmy` join `reżyser` on(`filmy`.`reżyser` = `reżyser`.`id`)) join `gatunek` on(`filmy`.`gatunek` = `gatunek`.`id`)) join `aktor` on(`filmy`.`główna_rola` = `aktor`.`id`)) join `wytwórnia` on(`filmy`.`wytwórnia` = `wytwórnia`.`id`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 
 --
 -- Final view structure for view `filmyl`
@@ -326,9 +363,10 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-21 13:47:35
+-- Dump completed on 2023-05-22 11:09:54
 
-create or replace procedure filmy()
+create
+    definer = root@localhost procedure filmy()
 begin
     drop view if exists filmyl;
     create view filmyl as SELECT tytul, gatunek.nazwa as gatunek, CONCAT(reżyser.Imie,' ',reżyser.nazwisko) as reżyser , wytwórnia.nazwa as wytwórnia, CONCAT(aktor.imie,' ',aktor.nazwisko) as rola_główna  from filmy
@@ -338,7 +376,19 @@ begin
     inner join wytwórnia on filmy.wytwórnia = wytwórnia.id;
 end;
 
-create or replace procedure kary()
+create
+    definer = root@localhost procedure filmy2()
+begin
+    drop view if exists filmy2;
+    create view filmy2 as SELECT filmy.id,tytul, gatunek.nazwa as gatunek, CONCAT(reżyser.Imie,' ',reżyser.nazwisko) as reżyser , wytwórnia.nazwa as wytwórnia, CONCAT(aktor.imie,' ',aktor.nazwisko) as rola_główna  from filmy
+    inner join reżyser on filmy.reżyser = reżyser.id
+    inner join gatunek on filmy.gatunek = gatunek.id
+    inner join aktor on filmy.główna_rola = aktor.id
+    inner join wytwórnia on filmy.wytwórnia = wytwórnia.id;
+end;
+
+create
+    definer = root@localhost procedure kary()
 begin
     drop view if exists kara;
     create view kara as SELECT filmy.tytul,concat(klienci.imie,' ',klienci.nazwisko) as klient,klienci.pesel as pesel, wypożyczenia.data_wypożyczenia, zwroty.data_zwrotu, DATEDIFF(zwroty.data_zwrotu,wypożyczenia.data_wypożyczenia) as dni_wypożyczenia, (DATEDIFF(zwroty.data_zwrotu,wypożyczenia.data_wypożyczenia)-wypożyczenia.długość) as po_terminie,
@@ -348,14 +398,34 @@ begin
     inner join klienci on   klienci.id = wypożyczenia.klient;
 end;
 
-create or replace procedure klienci()
+
+create
+    definer = root@localhost procedure klienci()
 begin
     SELECT CONCAT(imie,' ',nazwisko) as klient,adres,kod_pocztowy,miejscowość,nr_telefonu,mail from klienci;
 end;
 
-create or replace procedure wypożyczenia()
+
+
+create
+    definer = root@localhost procedure klienci2()
+begin
+    SELECT klienci.id,CONCAT(imie,' ',nazwisko) as klient,adres,kod_pocztowy,miejscowość,nr_telefonu,mail from klienci;
+end;
+
+create
+    definer = root@localhost procedure wypożyczenia()
 begin
     SELECT filmy.tytul,CONCAT(klienci.imie,' ',klienci.nazwisko) as klient, data_wypożyczenia, długość, cena from wypożyczenia
     inner join filmy on wypożyczenia.film = filmy.id
     inner join klienci on wypożyczenia.klient = klienci.id;
 end;
+
+create
+    definer = root@localhost procedure wypożyczenia2()
+begin
+    SELECT wypożyczenia.id,filmy.tytul,CONCAT(klienci.imie,' ',klienci.nazwisko) as klient, data_wypożyczenia, długość, cena from wypożyczenia
+    inner join filmy on wypożyczenia.film = filmy.id
+    inner join klienci on wypożyczenia.klient = klienci.id;
+end;
+
